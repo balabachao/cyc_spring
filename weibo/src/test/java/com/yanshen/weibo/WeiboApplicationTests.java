f534ec00b3d2b66d2920d47a4b80e7dd59023b75
cuiyanchao <balabachao@gmail.com>
2020-09-02 09:42:28 +0800

    initial

--- weibo/src/test/java/com/yanshen/weibo/WeiboApplicationTests.java
+++ weibo/src/test/java/com/yanshen/weibo/WeiboApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo3;
+package com.yanshen.weibo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Demo3ApplicationTests {
+class WeiboApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
