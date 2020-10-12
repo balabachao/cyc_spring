826cf44b37b498f38f5d1f74724ab43a8c407445
cuiyanchao <balabachao@gmail.com>
2020-10-12 09:04:03 +0800

    初始化

---
+++ sign/src/test/java/com/yanshen/sign/SignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanshen.sign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
