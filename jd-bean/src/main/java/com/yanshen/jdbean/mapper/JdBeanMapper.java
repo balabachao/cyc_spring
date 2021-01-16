d6dfdb7c40a30b8b2531af76453c829259eb8821
PC67 <balabachao@gmail.com>
2021-01-16 11:16:09 +0800

    查询所有code

--- jd-bean/src/main/java/com/yanshen/jdbean/mapper/JdBeanMapper.java
+++ jd-bean/src/main/java/com/yanshen/jdbean/mapper/JdBeanMapper.java
@@ -4,10 +4,11 @@ import com.yanshen.jdbean.utils.JdBean;
 
 import java.util.List;
 
-public interface JdBeanMapper {
+public interface JdBeanMapper  {
     void addBean(JdBean code);
     JdBean find(JdBean c);
     List<JdBean> findByType(Integer type);
     List<JdBean> findByIP(String ip);
+    List<JdBean> findAll();
     void update(List<JdBean> list);
 }
